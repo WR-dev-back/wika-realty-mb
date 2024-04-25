4a39f9896c3f23ee6e630eb9bbd3da9447e11acd
dawamuciil <dawamuciil@gmail.com>
2024-04-25 18:58:11 +0700

    initial commit

---
+++ lib/app/modules/splashscreen/controllers/splashscreen_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+import '../../../routes/app_pages.dart';
+
+class SplashscreenController extends GetxController {
+  final keyIntroScreen = GlobalKey<IntroductionScreenState>();
+
+  @override
+  void onClose() {}
+  void goToHome() => Get.offAllNamed(Routes.HOME);
+}
