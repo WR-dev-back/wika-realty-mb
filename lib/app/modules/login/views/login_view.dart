4d6886dbc02547ecbe4f5b2300c78612414204d1
dawamuciil <dawamuciil@gmail.com>
2024-04-29 08:50:42 +0700

    message

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -19,57 +19,88 @@ class LoginView extends GetView<LoginController> {
         onPressed: () {},
         icon: Icon(Icons.close),
       )),
-      body: Stack(
-        children: [
-          Column(children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 65 / 100,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.white,
-              padding: const EdgeInsets.only(
-                  left: 20, right: 20, top: 36, bottom: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10),
-                    child: const Text(
-                      'Masuk Akun',
+      body: ListView(children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 65 / 100,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.white,
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 36, bottom: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child: const Text(
+                  'Masuk Akun',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'poppins',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child: const Text(
+                  'Lengkapi data dibawah untuk masuk ke akunmu yang sudah terdaftar di We Stay.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'poppins',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.only(left: 14, right: 14, top: 4),
+                margin: const EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(width: 1, color: Colors.grey),
+                ),
+                child: TextField(
+                  style: const TextStyle(fontSize: 14, fontFamily: 'poppins'),
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Email",
                       style: TextStyle(
-                        fontSize: 25,
-                        fontFamily: 'poppins',
-                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 14,
                       ),
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10),
-                    child: const Text(
-                      'Lengkapi data dibawah untuk masuk ke akunmu yang sudah terdaftar di We Stay.',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'poppins',
-                        fontWeight: FontWeight.w400,
-                      ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    border: InputBorder.none,
+                    hintText: "youremail@email.com",
+                    hintStyle: TextStyle(
+                      fontSize: 14,
+                      fontFamily: 'poppins',
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
                     ),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: const EdgeInsets.only(left: 14, right: 14, top: 4),
-                    margin: const EdgeInsets.only(bottom: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(width: 1, color: Colors.grey),
-                    ),
-                    child: TextField(
+                ),
+              ),
+              Material(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.only(left: 14, right: 14, top: 4),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(width: 1, color: Colors.grey),
+                  ),
+                  child: Obx(
+                    () => TextField(
                       style:
                           const TextStyle(fontSize: 14, fontFamily: 'poppins'),
                       maxLines: 1,
+                      obscureText: controller.obsecureText.value,
                       decoration: InputDecoration(
                         label: Text(
-                          "Email",
+                          "Password",
                           style: TextStyle(
                             color: Colors.grey,
                             fontSize: 14,
@@ -77,7 +108,16 @@ class LoginView extends GetView<LoginController> {
                         ),
                         floatingLabelBehavior: FloatingLabelBehavior.always,
                         border: InputBorder.none,
-                        hintText: "youremail@email.com",
+                        hintText: "*************",
+                        suffixIcon: IconButton(
+                          icon: (controller.obsecureText != false)
+                              ? SvgPicture.asset('assets/icons/show.svg')
+                              : SvgPicture.asset('assets/icons/hide.svg'),
+                          onPressed: () {
+                            controller.obsecureText.value =
+                                !(controller.obsecureText.value);
+                          },
+                        ),
                         hintStyle: TextStyle(
                           fontSize: 14,
                           fontFamily: 'poppins',
@@ -87,144 +127,96 @@ class LoginView extends GetView<LoginController> {
                       ),
                     ),
                   ),
-                  Material(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      padding:
-                          const EdgeInsets.only(left: 14, right: 14, top: 4),
-                      decoration: BoxDecoration(
+                ),
+              ),
+              Container(
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.blue,
+                  ),
+                  child: const Text("Lupa password?"),
+                ),
+              ),
+              Obx(
+                () => SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    onPressed: () => Get.offAllNamed(Routes.HOME),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 18),
+                      backgroundColor: Colors.grey,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(8),
-                        border: Border.all(width: 1, color: Colors.grey),
-                      ),
-                      child: Obx(
-                        () => TextField(
-                          style: const TextStyle(
-                              fontSize: 14, fontFamily: 'poppins'),
-                          maxLines: 1,
-                          obscureText: controller.obsecureText.value,
-                          decoration: InputDecoration(
-                            label: Text(
-                              "Password",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 14,
-                              ),
-                            ),
-                            floatingLabelBehavior: FloatingLabelBehavior.always,
-                            border: InputBorder.none,
-                            hintText: "*************",
-                            suffixIcon: IconButton(
-                              icon: (controller.obsecureText != false)
-                                  ? SvgPicture.asset('assets/icons/show.svg')
-                                  : SvgPicture.asset('assets/icons/hide.svg'),
-                              onPressed: () {
-                                controller.obsecureText.value =
-                                    !(controller.obsecureText.value);
-                              },
-                            ),
-                            hintStyle: TextStyle(
-                              fontSize: 14,
-                              fontFamily: 'poppins',
-                              fontWeight: FontWeight.w500,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ),
                       ),
                     ),
+                    child: Text(
+                      (controller.isLoading.isFalse) ? 'Log in' : 'Loading...',
+                      style: const TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'poppins',
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
                   ),
+                ),
+              ),
+              SizedBox(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 18),
+                  child: Center(
+                    child: Text('atau masuk dengan'),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                   Container(
-                    child: TextButton(
+                    width: MediaQuery.of(context).size.width * 0.42,
+                    child: ElevatedButton(
                       onPressed: () {},
-                      style: TextButton.styleFrom(
-                        foregroundColor: Colors.blue,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        backgroundColor: Colors.grey,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
-                      child: const Text("Lupa password?"),
+                      child: Text('Facebook'),
                     ),
                   ),
-                  Obx(
-                    () => SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: ElevatedButton(
-                        onPressed: () => Get.offAllNamed(Routes.HOME),
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 18),
-                          backgroundColor: Colors.grey,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        child: Text(
-                          (controller.isLoading.isFalse)
-                              ? 'Log in'
-                              : 'Loading...',
-                          style: const TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'poppins',
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black),
+                  SizedBox(width: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.42,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        backgroundColor: Colors.grey,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
                         ),
                       ),
+                      child: Text('Google'),
                     ),
                   ),
-                  SizedBox(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 10, bottom: 18),
-                      child: Center(
-                        child: Text('atau masuk dengan'),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.42,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(vertical: 15),
-                            backgroundColor: Colors.grey,
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          child: Text('Facebook'),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.42,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(vertical: 15),
-                            backgroundColor: Colors.grey,
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          child: Text('Google'),
-                        ),
-                      ),
-                    ],
-                  )
                 ],
-              ),
-            ),
-            Expanded(
-              child: Image.asset(
-                'assets/images/background.png',
-                fit: BoxFit.fitWidth,
-                alignment: Alignment.bottomCenter,
-              ),
-            ),
-          ]),
-        ],
-      ),
+              )
+            ],
+          ),
+        ),
+        Container(
+          child: Image.asset(
+            'assets/images/background.png',
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+      ]),
     );
   }
 }
